446a34eccff939bc179b0b83f79ff2a0d144e3d1
Neptune669 <89285566+Neptune669@users.noreply.github.com>
2024-04-06 17:42:51 +0200

    prod

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,7 +6,6 @@ import { redirect } from "next/navigation";
 import { SearchCommand } from "@/components/search-command";
 
 import { Spinner } from "@/components/ui/sppiner";
-import Nav from "./_components/navigation";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -25,7 +24,6 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="h-full flex dark:bg-[#1F1F1F]">
-      <Nav />
       <main className="flex-1 h-full overflow-y-auto">
         <SearchCommand />
         {children}
